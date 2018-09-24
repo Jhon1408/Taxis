a98219356d7f66cd477079d94e2abc51e0f4f20c
Jhonatan Ospina <wother1409@gmail.com>
2018-09-23 23:27:23 -0500

    Clase Conexion.

---
+++ src/taxis/Conexion.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2018 jhonatan.
+ *
+ * you may not use this file except in compliance with the Owner.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package taxis;
+
+/**
+ *
+ * @author jhonatan
+ */
+public class Conexion {
+    /*
+        Creacion de la clase conexion para usar una base de datos
+        alojada en localhost/xampp
+    */
+}
